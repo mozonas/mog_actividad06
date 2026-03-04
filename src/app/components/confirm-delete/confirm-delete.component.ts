f94c325d5e7479c0148eaf87777be4aafd3722e2
Miguel <miguelozonas@gmail.com>
2026-03-04 10:55:49 +0100

    Primer commit

---
+++ src/app/components/confirm-delete/confirm-delete.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-delete',
+  imports: [],
+  templateUrl: './confirm-delete.component.html',
+  styleUrl: './confirm-delete.component.css',
+})
+export class ConfirmDeleteComponent {
+
+}
