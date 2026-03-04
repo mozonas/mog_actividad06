b3adcef68c6157f17ba55a89749dcf10caec495f
Miguel <miguelozonas@gmail.com>
2026-03-04 12:47:11 +0100

    creación de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component'; 
+import { UserDetailComponent } from './components/user-detail/user-detail.component'; 
+import { UserFormComponent } from './components/user-form/user-form.component'; 
+
+export const routes: Routes = [ 
+    { path: 'home', component: HomeComponent }, 
+    { path: 'user/:id', component: UserDetailComponent }, 
+    { path: 'newuser', component: UserFormComponent }, 
+    { path: 'updateuser/:id', component: UserFormComponent }, 
+    { path: '', redirectTo: 'home', pathMatch: 'full' }, 
+    { path: '**', redirectTo: 'home' } ];
